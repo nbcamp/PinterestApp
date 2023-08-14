1eae4c551f855bdbe8594e0e873ae98cceaf7f42
jinyongp <dev.jinyongp@gmail.com>
2023-08-14 20:08:45 +0900

    feat: collectionView 추가

--- PinterestApp/ViewController.swift
+++ PinterestApp/ViewController.swift
@@ -1,12 +1,22 @@
 import UIKit
 
-class ViewController: UIViewController {
-    let label = {
-        let label = UILabel()
-        label.text = "Hello, World!"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+struct ImageData {
+    let url: String
+    let width: Int
+    let height: Int
+}
+
+final class ViewController: UIViewController {
+    let images: [ImageData] = (1 ... 100).map {
+        let (width, height) = (200, Int.random(in: 2...4) * 100)
+        return ImageData(
+            url: "https://picsum.photos/seed/\($0)/\(width)/\(height)",
+            width: width,
+            height: height
+        )
+    }
+
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,22 +26,101 @@ class ViewController: UIViewController {
     func initializeUI() {
         view.backgroundColor = .secondarySystemBackground
 
-        view.addSubview(label)
+        setupCollectionView()
+    }
 
-        let layout = view.safeAreaLayoutGuide
+    func setupCollectionView() {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        view.addSubview(collectionView)
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: layout.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: layout.centerYAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(ImageCell.self, forCellWithReuseIdentifier: "cell")
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ImageCell else {
+            return UICollectionViewCell()
+        }
+        let image = images[indexPath.row]
+        cell.imageView.load(url: URL(string: image.url)!)
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let columns = 2.0
+        let spacing = 10.0
+        let margin = 10.0
+        let width = (collectionView.bounds.width - ((columns - 1) * (spacing + margin * 2))) / columns
+        let size = CGSize(width: width, height: width)
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 10, left: 10, bottom: 10, right: 10)
+    }
+}
+
+class ImageCell: UICollectionViewCell {
+    let imageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+
+        contentMode = .scaleAspectFit
+        layer.cornerRadius = 5.0
+        layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
-}
 
-#if canImport(SwiftUI) && DEBUG
-import SwiftUI
-
-struct PreView: PreviewProvider {
-    static var previews: some View {
-        ViewController().toPreview()
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
 }
-#endif
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            guard let data = try? Data(contentsOf: url) else { return }
+            guard let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async { self?.image = image }
+        }
+    }
+}
+
+//
+// #if canImport(SwiftUI) && DEBUG
+// import SwiftUI
+//
+// struct PreView: PreviewProvider {
+//    static var previews: some View {
+//        ViewController().toPreview()
+//    }
+// }
+// #endif

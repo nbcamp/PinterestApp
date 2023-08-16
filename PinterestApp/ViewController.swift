4cfeea4819fb5a4fe8381d40a31a09be6005219f
jinyongp <dev.jinyongp@gmail.com>
2023-08-16 15:15:49 +0900

    init: 프로젝트 생성 및 초기화 & Main.storyboard 제거

---
+++ PinterestApp/ViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeUI()
+    }
+
+    func initializeUI() {
+        view.backgroundColor = .secondarySystemBackground
+
+        let button = {
+            let button = UIButton()
+            button.setTitle("Button", for: .normal)
+            button.setTitleColor(.systemBlue, for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }()
+
+        view.addSubview(button)
+
+        let layout = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: layout.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: layout.centerYAnchor),
+        ])
+    }
+
+    @objc
+    func buttonTapped() {
+        print("화면 전환 테스트 코드가 필요하다면 여기 작성하세요.")
+        print("Commit하지 않도록 주의하세요.")
+    }
+}

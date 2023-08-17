d95e86bce5f999c86a2604f8ee78f1085888f245
jinyongp <dev.jinyongp@gmail.com>
2023-08-17 19:48:52 +0900

    feat: 코드 정리 및 개선

--- PinterestApp/Views/PinterestCollectionViewFlowLayout.swift
+++ PinterestApp/Views/PinterestCollectionViewFlowLayout.swift
@@ -8,6 +8,7 @@ import UIKit
 final class PinterestCollectionViewFlowLayout: UICollectionViewFlowLayout {
     var numberOfColumns = 1
     var contentPadding: CGFloat = 0
+
     weak var delegate: PinterestCollectionViewDelegateFlowLayout?
 
     private var attributesCache: [UICollectionViewLayoutAttributes] = []
@@ -20,34 +21,32 @@ final class PinterestCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
     override func prepare() {
         super.prepare()
-
         guard let collectionView, attributesCache.isEmpty else { return }
 
         columnHeights = .init(repeating: 0, count: numberOfColumns)
         attributesCache = []
 
         for section in 0..<collectionView.numberOfSections {
-            let contentPadding = delegate?.collectionView?(collectionView, layout: self, contentPaddingForSectionAt: section) ?? self.contentPadding
-            let columnWidth = (collectionView.bounds.width - (CGFloat(numberOfColumns + 1) * contentPadding)) / CGFloat(numberOfColumns)
-            let offsets: [CGFloat] = (0..<numberOfColumns).map { CGFloat($0) * (columnWidth + contentPadding) + contentPadding }
+            let contentPadding = delegate?.collectionView?(collectionView, layout: self, contentPaddingForSectionAt: section) ?? contentPadding
+            let contentWidth = (collectionView.bounds.width - (CGFloat(numberOfColumns + 1) * contentPadding)) / CGFloat(numberOfColumns)
+            let columnOffsets: [CGFloat] = (0..<numberOfColumns).map { CGFloat($0) * (contentWidth + contentPadding) + contentPadding }
 
             var column = 0
             for item in 0..<collectionView.numberOfItems(inSection: section) {
                 let indexPath = IndexPath(item: item, section: section)
-                let contentHeight = contentPadding * 2 + (delegate?.collectionView?(collectionView, layout: self, contentHeightAt: indexPath) ?? columnWidth)
-                let frame = CGRect(x: offsets[column], y: columnHeights[column], width: columnWidth, height: contentHeight)
+                let contentHeight = contentPadding * 2 + (delegate?.collectionView?(collectionView, layout: self, contentHeightAt: indexPath) ?? contentWidth)
                 let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-                attributes.frame = frame
+                attributes.frame = CGRect(x: columnOffsets[column], y: columnHeights[column], width: contentWidth, height: contentHeight)
                 attributesCache.append(attributes)
 
                 columnHeights[column] = columnHeights[column] + contentHeight + contentPadding
-                column = (column >= (numberOfColumns - 1)) ? 0 : column + 1
+                column = (column + 1) % numberOfColumns
             }
         }
     }
 
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        attributesCache.compactMap { $0.frame.intersects(rect) ? $0 : nil }
+        attributesCache.filter { $0.frame.intersects(rect) }
     }
 
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {

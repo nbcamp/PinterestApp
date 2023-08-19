e48fdb3eeddcdf007af76e2451fbc408512dad5c
osrsla <139095139+osrsla@users.noreply.github.com>
2023-08-19 15:47:58 +0900

    프로필 페이지 그리드 레이아웃 추가 작업 (#11)

--- PinterestApp/Controllers/ProfileViewController.swift
+++ PinterestApp/Controllers/ProfileViewController.swift
@@ -4,35 +4,29 @@ final class ProfileViewController: UIViewController {
     private let scrollView = UIScrollView()
     private let stackView = UIStackView()
 
-    let profileView = ProfileView()
+    let userImage = UIImageView(image: UIImage(named: "5"))
+    let userNameLabel = UILabel()
+    let userDetail = UILabel()
+
     let editProfileButton = UIButton(type: .system)
 
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.allowsSelection = true
-        tableView.register(ProfileCustomCell.self, forCellReuseIdentifier: ProfileCustomCell.identifier)
+    private let subStackView = UIStackView()
+    private let userCreatedLabel = UILabel()
+    private let gridButton = UIButton(type: .system)
+    private let plusButton = UIButton(type: .system)
 
-        return tableView
-    }()
+    private var collectionView: UICollectionView!
 
-    let userCreatedLabel = UILabel()
+    var columns = 2
 
-    private let images: [UIImage] = [
-        UIImage(named: "1")!,
-        UIImage(named: "2")!,
-        UIImage(named: "3")!,
-        UIImage(named: "4")!,
-        UIImage(named: "5")!
-    ]
+    private var media: [Medium] { MediumService.shared.media }
+
+    private var currentGridType: GridType = .square2x2
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupStyle()
         setupLayout()
-
-        tableView.dataSource = self
-        tableView.delegate = self
     }
 
     override func viewDidLayoutSubviews() {
@@ -49,15 +43,38 @@ extension ProfileViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
 
-        profileView.translatesAutoresizingMaskIntoConstraints = false
+        userImage.translatesAutoresizingMaskIntoConstraints = false
+        userImage.backgroundColor = .systemGray
+        userImage.contentMode = .scaleAspectFill
+        userImage.clipsToBounds = true
+        userImage.layer.cornerRadius = 75
+        userImage.layer.shadowOffset = CGSize(width: 5, height: 5)
+        userImage.layer.shadowOpacity = 0.7
+        userImage.layer.shadowRadius = 5
+        userImage.layer.shadowColor = UIColor.gray.cgColor
+
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        userNameLabel.textAlignment = .center
+        userNameLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle).withSize(48)
+        userNameLabel.adjustsFontForContentSizeCategory = true
+        userNameLabel.text = "Sixteen"
+
+        userDetail.translatesAutoresizingMaskIntoConstraints = false
+        userDetail.textAlignment = .center
+        userDetail.font = UIFont.preferredFont(forTextStyle: .caption2).withSize(16)
+        userDetail.adjustsFontForContentSizeCategory = true
+        userDetail.numberOfLines = 0
+        userDetail.text = "@user 🌿 Welcome!!"
 
         editProfileButton.translatesAutoresizingMaskIntoConstraints = false
         editProfileButton.backgroundColor = .systemGray
         editProfileButton.setTitle("Edit", for: .normal)
         editProfileButton.setTitleColor(.darkText, for: .normal)
         editProfileButton.layer.cornerRadius = 5
+        editProfileButton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
 
-        editProfileButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        subStackView.translatesAutoresizingMaskIntoConstraints = false
+        subStackView.axis = .horizontal
 
         userCreatedLabel.translatesAutoresizingMaskIntoConstraints = false
         userCreatedLabel.textAlignment = .center
@@ -65,17 +82,43 @@ extension ProfileViewController {
         userCreatedLabel.adjustsFontForContentSizeCategory = true
         userCreatedLabel.text = "Created"
 
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(ProfileCustomCell.self, forCellReuseIdentifier: ProfileCustomCell.identifier)
+        gridButton.translatesAutoresizingMaskIntoConstraints = false
+        gridButton.setImage(UIImage(systemName: "square.grid.2x2.fill"), for: .normal)
+        gridButton.tintColor = .darkText
+        gridButton.addTarget(self, action: #selector(gridButtonTapped), for: .touchUpInside)
+
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        plusButton.tintColor = .darkText
+        plusButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
+
+        let layout = PinterestCollectionViewFlowLayout()
+        layout.numberOfColumns = columns
+        layout.delegate = self
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(
+            ImageCollectionViewCell.self,
+            forCellWithReuseIdentifier: ImageCollectionViewCell.identifier
+        )
     }
 
     private func setupLayout() {
         view.addSubview(scrollView)
+
         scrollView.addSubview(stackView)
-        stackView.addSubview(profileView)
+
+        stackView.addSubview(userImage)
+        stackView.addSubview(userNameLabel)
+        stackView.addSubview(userDetail)
         stackView.addSubview(editProfileButton)
-        stackView.addSubview(userCreatedLabel)
-        stackView.addSubview(tableView)
+        stackView.addSubview(subStackView)
+        subStackView.addSubview(userCreatedLabel)
+        subStackView.addSubview(gridButton)
+        subStackView.addSubview(plusButton)
+        stackView.addSubview(collectionView)
 
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -89,58 +132,124 @@ extension ProfileViewController {
             stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
 
-            profileView.topAnchor.constraint(equalTo: stackView.topAnchor),
-            profileView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
-            profileView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
-            profileView.heightAnchor.constraint(equalToConstant: 220),
+            userImage.topAnchor.constraint(equalToSystemSpacingBelow: stackView.topAnchor, multiplier: 3),
+            userImage.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            userImage.widthAnchor.constraint(equalToConstant: 150),
+            userImage.heightAnchor.constraint(equalToConstant: 150),
 
-            editProfileButton.topAnchor.constraint(equalToSystemSpacingBelow: profileView.bottomAnchor, multiplier: 3),
+            userNameLabel.topAnchor.constraint(equalToSystemSpacingBelow: userImage.bottomAnchor, multiplier: 1.5),
+            userNameLabel.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+
+            userDetail.topAnchor.constraint(equalToSystemSpacingBelow: userNameLabel.bottomAnchor, multiplier: 1.5),
+            userDetail.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            userDetail.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.6),
+
+            editProfileButton.topAnchor.constraint(equalToSystemSpacingBelow: userDetail.bottomAnchor, multiplier: 1.5),
             editProfileButton.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
             editProfileButton.widthAnchor.constraint(equalTo: editProfileButton.titleLabel!.widthAnchor, constant: 16),
 
-            userCreatedLabel.topAnchor.constraint(equalToSystemSpacingBelow: editProfileButton.bottomAnchor, multiplier: 5),
-            userCreatedLabel.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
+            subStackView.topAnchor.constraint(equalToSystemSpacingBelow: editProfileButton.bottomAnchor, multiplier: 3),
+            subStackView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            subStackView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            subStackView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            subStackView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+
+            userCreatedLabel.topAnchor.constraint(equalTo: subStackView.topAnchor),
+            userCreatedLabel.centerXAnchor.constraint(equalTo: subStackView.centerXAnchor),
             userCreatedLabel.widthAnchor.constraint(equalToConstant: 100),
 
-            tableView.topAnchor.constraint(equalToSystemSpacingBelow: userCreatedLabel.bottomAnchor, multiplier: 1),
-            tableView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 10),
-            tableView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -10),
-            tableView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
-            tableView.heightAnchor.constraint(equalTo: tableView.widthAnchor, multiplier: CGFloat(images.count))
+            gridButton.topAnchor.constraint(equalTo: subStackView.topAnchor),
+            gridButton.trailingAnchor.constraint(equalTo: plusButton.leadingAnchor, constant: -1),
+            gridButton.heightAnchor.constraint(equalTo: userCreatedLabel.heightAnchor),
+            gridButton.widthAnchor.constraint(equalTo: gridButton.heightAnchor),
 
+            plusButton.topAnchor.constraint(equalTo: subStackView.topAnchor),
+            plusButton.trailingAnchor.constraint(equalTo: subStackView.trailingAnchor, constant: -22),
+            plusButton.heightAnchor.constraint(equalTo: userCreatedLabel.heightAnchor),
+            plusButton.widthAnchor.constraint(equalTo: plusButton.heightAnchor),
+
+            collectionView.topAnchor.constraint(equalToSystemSpacingBelow: plusButton.bottomAnchor, multiplier: 1),
+            collectionView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: -10),
+            collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 3),
         ])
+    }
+}
+
+extension ProfileViewController: UICollectionViewDelegate {}
+
+extension ProfileViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return media.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let medium = media[indexPath.item]
+        cell.imageView.image = medium.image
+        return cell
+    }
+}
+
+extension ProfileViewController: PinterestCollectionViewDelegateFlowLayout {
+    var contentPadding: CGFloat { 10.0 }
+
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, contentHeightAt indexPath: IndexPath) -> CGFloat {
+        let image = media[indexPath.item]
+        let width = (collectionView.bounds.width - (CGFloat(columns + 1) * contentPadding)) / CGFloat(columns)
+        return width * (Double(image.height) / Double(image.width))
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, contentPaddingForSectionAt section: Int) -> CGFloat {
+        return contentPadding
     }
 }
 
 extension ProfileViewController {
     @objc
-    private func buttonTapped() {
+    private func editButtonTapped() {
         navigationController?.pushViewController(EditProfileViewController(), animated: true)
     }
-}
 
-extension ProfileViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let cellSpacing: CGFloat = 5
-        let screenWidth = tableView.bounds.width
-        let cellHeight = screenWidth * 0.9
-        return cellHeight + cellSpacing
-    }
-}
-
-extension ProfileViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
+    private enum GridType {
+        case square3x3, square1x1, square2x2
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileCustomCell.identifier, for: indexPath) as? ProfileCustomCell else {
-            fatalError("The TableView could not dequeue a CustomCell in ViewController.")
+    @objc
+    private func gridButtonTapped() {
+        switch currentGridType {
+            case .square3x3:
+                currentGridType = .square1x1
+                gridButton.setImage(UIImage(systemName: "square.fill"), for: .normal)
+                columns = 1
+            case .square1x1:
+                currentGridType = .square2x2
+                gridButton.setImage(UIImage(systemName: "square.grid.2x2.fill"), for: .normal)
+                columns = 2
+            case .square2x2:
+                currentGridType = .square3x3
+                gridButton.setImage(UIImage(systemName: "square.grid.3x3.fill"), for: .normal)
+                columns = 3
         }
 
-        let image = images[indexPath.row]
-        cell.configure(with: image)
+        updateCollectionViewLayout()
+    }
 
-        return cell
+    private func updateCollectionViewLayout() {
+        let layout = collectionView.collectionViewLayout as! PinterestCollectionViewFlowLayout
+        layout.numberOfColumns = columns
+        layout.invalidateLayout()
+
+        collectionView.reloadData()
+        print("CollectionView columns: \(columns)")
+    }
+
+    @objc
+    private func plusButtonTapped() {
+        navigationController?.pushViewController(NewPostViewController(), animated: true)
     }
 }

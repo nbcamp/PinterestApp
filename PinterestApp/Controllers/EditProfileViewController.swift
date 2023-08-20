5dc194e578281c17fd20040e1ec8044c61f98bb9
원성준 <139090550+dnjs012452@users.noreply.github.com>
2023-08-20 11:26:06 +0900

    프로필 수정 페이지 레이아웃 개선 및 키보드 위치 대응 (#12)

--- PinterestApp/Controllers/EditProfileViewController.swift
+++ PinterestApp/Controllers/EditProfileViewController.swift
@@ -2,7 +2,23 @@ import UIKit
 
 final class EditProfileViewController: UIViewController {
     private var loadingView: UIView?
+   
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
     
+    let contentView: UIStackView = {
+        let contentView = UIStackView()
+        contentView.axis = .vertical
+        contentView.spacing = 20
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return contentView
+    }()
+
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .white
@@ -42,17 +58,20 @@ final class EditProfileViewController: UIViewController {
         return nameLabel
     }()
     
-    let firstNameTextField: UITextField = {
+    let nameTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.placeholder = "name"
-        textField.font = UIFont.systemFont(ofSize: 20)
-        textField.borderStyle = .roundedRect
-        // textField.layer.cornerRadius = 20.0
+        textField.font = UIFont.systemFont(ofSize: 19)
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.layer.cornerRadius = 13.0
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
         
         return textField
     }()
-   
+
     let introduceLabel: UILabel = {
         let introduceLabel = UILabel()
         introduceLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -70,15 +89,15 @@ final class EditProfileViewController: UIViewController {
         textView.font = UIFont.systemFont(ofSize: 17)
         textView.layer.borderColor = UIColor.lightGray.cgColor
         textView.layer.borderWidth = 1.0
-        textView.layer.cornerRadius = 10.0
-        textView.textContainerInset = UIEdgeInsets(top: 12, left: 7, bottom: 12, right: 7)
+        textView.layer.cornerRadius = 15.0
+        textView.textContainerInset = UIEdgeInsets(top: 12, left: 5, bottom: 12, right: 5)
         textView.textColor = .placeholderText
         
         textView.delegate = self
         
         return textView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -88,6 +107,12 @@ final class EditProfileViewController: UIViewController {
         setUpImagePickerController()
         setUpViews()
         setUpConstraints()
+        
+        nameTextField.delegate = self
+        introduceTextView.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     private func addNavButtons() {
@@ -100,7 +125,7 @@ final class EditProfileViewController: UIViewController {
               
         let titleTextAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.boldSystemFont(ofSize: 21),
-            .foregroundColor: UIColor.label
+            .foregroundColor: UIColor.label,
         ]
         navigationController?.navigationBar.titleTextAttributes = titleTextAttributes
         navigationItem.title = "Edit profile"
@@ -108,6 +133,7 @@ final class EditProfileViewController: UIViewController {
     
     private func initializeUI() {
         view.backgroundColor = .systemBackground
+        scrollView.alwaysBounceVertical = true
     }
 
     private func setUpImagePickerController() {
@@ -117,54 +143,23 @@ final class EditProfileViewController: UIViewController {
     }
     
     private func setUpViews() {
-        // view.addSubview(navigationBar)
-        view.addSubview(imageView)
-        view.addSubview(changeButton)
-        view.addSubview(nameLabel)
-        view.addSubview(firstNameTextField)
-        view.addSubview(introduceLabel)
-        view.addSubview(introduceTextView)
-    }
-    
-    private func setUpConstraints() {
-        let safeArea = view.safeAreaLayoutGuide
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         
-        NSLayoutConstraint.activate([
-            // 프로필 이미지뷰
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 150),
-            imageView.heightAnchor.constraint(equalToConstant: 150),
-            imageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8.0),
-            
-            // 프로필 사진 변경 버튼
-            changeButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 14.0),
-            changeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            // name 라벨
-            nameLabel.topAnchor.constraint(equalTo: changeButton.bottomAnchor, constant: 15.0),
-            nameLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 35.0),
-            nameLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30.0),
-            nameLabel.heightAnchor.constraint(equalToConstant: 20.0),
-            
-            // first name 텍스트 필드
-            firstNameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 16.0),
-            firstNameTextField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 30.0),
-            firstNameTextField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30.0),
-            firstNameTextField.heightAnchor.constraint(equalToConstant: 50.0),
-            
-            // introduce 라벨
-            introduceLabel.topAnchor.constraint(equalTo: firstNameTextField.bottomAnchor, constant: 25.0),
-            introduceLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 35.0),
-            introduceLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30.0),
-            introduceLabel.heightAnchor.constraint(equalToConstant: 20.0),
-            
-            // introduce 텍스트 뷰
-            introduceTextView.topAnchor.constraint(equalTo: introduceLabel.bottomAnchor, constant: 16.0),
-            introduceTextView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 30.0),
-            introduceTextView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30.0),
-            introduceTextView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -24.0)
-            
-        ])
+        let imageStackView = UIStackView(arrangedSubviews: [imageView, changeButton])
+        imageStackView.axis = .vertical
+        imageStackView.spacing = 10
+        contentView.addArrangedSubview(imageStackView)
+                
+        let nameStackView = UIStackView(arrangedSubviews: [nameLabel, nameTextField])
+        nameStackView.axis = .vertical
+        nameStackView.spacing = 10
+        contentView.addArrangedSubview(nameStackView)
+                
+        let introduceStackView = UIStackView(arrangedSubviews: [introduceLabel, introduceTextView])
+        introduceStackView.axis = .vertical
+        introduceStackView.spacing = 10
+        contentView.addArrangedSubview(introduceStackView)
     }
     
     @objc func backButtonTap() {
@@ -178,7 +173,7 @@ final class EditProfileViewController: UIViewController {
             self.dismissLoadingScreen()
         }
     }
-    
+
     @objc func changeButtonTap() {
         present(imagePickerController, animated: true, completion: nil)
     }
@@ -207,6 +202,85 @@ final class EditProfileViewController: UIViewController {
         
         self.loadingView = nil
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameTextField.becomeFirstResponder()
+        introduceTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        hideKeyboardTappedAround()
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            let desiredKeyboardHeight: CGFloat = keyboardHeight
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: desiredKeyboardHeight, right: 0.0)
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
+        }
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+
+    // 레이아웃
+    private func setUpConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        imageView.leadingAnchor.constraint(greaterThanOrEqualTo: contentView.leadingAnchor, constant: 100.0).isActive = true
+        imageView.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -100.0).isActive = true
+        changeButton.leadingAnchor.constraint(greaterThanOrEqualTo: contentView.leadingAnchor, constant: 30.0).isActive = true
+        changeButton.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -30.0).isActive = true
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 40.0),
+            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            changeButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            changeButton.widthAnchor.constraint(equalToConstant: 100),
+            changeButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            nameLabel.topAnchor.constraint(equalTo: changeButton.bottomAnchor, constant: 60.0),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20.0),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 35.0),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30.0),
+            
+            nameTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30.0),
+            nameTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30.0),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40.0),
+            
+            introduceLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 35.0),
+            introduceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30.0),
+            introduceLabel.heightAnchor.constraint(equalToConstant: 20.0),
+                        
+            introduceTextView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30.0),
+            introduceTextView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30.0),
+            introduceTextView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -50.0),
+            introduceTextView.heightAnchor.constraint(equalToConstant: 220.0),
+            
+        ])
+    }
 }
 
 extension EditProfileViewController: UIImagePickerControllerDelegate {
@@ -230,7 +304,7 @@ extension EditProfileViewController: UIImagePickerControllerDelegate {
 extension EditProfileViewController: UINavigationControllerDelegate {
     //
 }
-	
+    
 extension EditProfileViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
         guard introduceTextView.textColor == .placeholderText else { return }
@@ -243,5 +317,43 @@ extension EditProfileViewController: UITextViewDelegate {
             introduceTextView.text = "introduce"
             introduceTextView.textColor = .placeholderText
         }
+    }
+}
+
+extension EditProfileViewController {
+    func hideKeyboardTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(EditProfileViewController
+                .dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string == "\n" {
+            textField.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+}
+
+extension EditProfileViewController: UITextFieldDelegate {
+    func textFieldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        introduceTextView.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        return true
     }
 }
